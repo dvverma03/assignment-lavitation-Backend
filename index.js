6eab8be898d12f9972d75493f5a7e742c0e148d5
dvverma03 <116081426+dvverma03@users.noreply.github.com>
2024-03-15 03:21:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 // };
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000/');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
