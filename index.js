d57c68ce9042cbd62ac96d95856e3e325576e73a
dvverma03 <116081426+dvverma03@users.noreply.github.com>
2024-03-14 23:12:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,20 @@ const corsOptions = {
   optionSuccessStatus: 200,
 };
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
+
 app.use(cors(corsOptions));
+
 app.use(CookieParser());
 
 mongoose
